96a3a41d9d2ebec1fd70581d22452a0001f7bfa4
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2015-03-03 23:32:47 +0100

    Completee rewrite. It's no longer coremod. 1.8 compatibility. Fixes #1

---
+++ src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
@@ -0,0 +1,20 @@
+package com.github.barteks2x.fastflybreak;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME)
+public class FFBB {
+  public static final String VERSION = "1.0";
+  public static final String NAME = "FastFlyBlockBreaking";
+  public static final String MODID = "fastflyblockbreaking";
+
+  private EventListener eventListener;
+
+  @Mod.EventHandler
+  public void init(FMLInitializationEvent event) {
+    this.eventListener = new EventListener();
+    MinecraftForge.EVENT_BUS.register(this.eventListener);
+  }
+}
