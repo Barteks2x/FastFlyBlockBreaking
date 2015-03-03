938a5344a7384e32e5086b7d343b4f65b3a606e4
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2015-03-04 00:47:32 +0100

    Attempt to make it possible to use version from git describe in jenkins builds.

--- src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
+++ src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME)
 public class FFBB {
-  public static final String VERSION = "1.0";
+  public static final String VERSION = "${version}";
   public static final String NAME = "FastFlyBlockBreaking";
   public static final String MODID = "fastflyblockbreaking";
 
