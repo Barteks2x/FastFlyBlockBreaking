be8a9d9708eec04566ff077862c5fe94cb8a9f30
Barteks2x <Barteks2x@gmail.com>
2019-04-17 12:21:46 +0200

    MC 1.8.9, simplify the code

--- src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
+++ src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
@@ -1,8 +1,10 @@
 package com.github.barteks2x.fastflybreak;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME)
 public class FFBB {
@@ -10,11 +12,15 @@ public class FFBB {
   public static final String NAME = "FastFlyBlockBreaking";
   public static final String MODID = "fastflyblockbreaking";
 
-  private EventListener eventListener;
-
   @Mod.EventHandler
   public void init(FMLInitializationEvent event) {
-    this.eventListener = new EventListener();
-    MinecraftForge.EVENT_BUS.register(this.eventListener);
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  @SubscribeEvent
+  public void blockBreakSpeed(PlayerEvent.BreakSpeed event){
+    if(!event.entityPlayer.onGround && event.entityPlayer.capabilities.isFlying){
+      event.newSpeed = event.originalSpeed * 5;
+    }
   }
 }
