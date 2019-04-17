097700cc38008d38e3a4dfbc1d349fc86c078a27
Barteks2x <Barteks2x@gmail.com>
2019-04-17 12:07:57 +0200

    Backport to 1.7.10

--- src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
+++ src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
@@ -1,20 +1,27 @@
 package com.github.barteks2x.fastflybreak;
 
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.Mod;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 
-@Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME)
+@Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME, )
 public class FFBB {
   public static final String VERSION = "${version}";
   public static final String NAME = "FastFlyBlockBreaking";
   public static final String MODID = "fastflyblockbreaking";
 
-  private EventListener eventListener;
-
   @Mod.EventHandler
   public void init(FMLInitializationEvent event) {
-    this.eventListener = new EventListener();
-    MinecraftForge.EVENT_BUS.register(this.eventListener);
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  @SubscribeEvent
+  public void blockBreakSpeed(PlayerEvent.BreakSpeed event){
+    event.entityPlayer.capabilities.allowFlying = true;
+    if(!event.entityPlayer.onGround && event.entityPlayer.capabilities.isFlying){
+      event.newSpeed = event.originalSpeed * 5;
+    }
   }
 }
