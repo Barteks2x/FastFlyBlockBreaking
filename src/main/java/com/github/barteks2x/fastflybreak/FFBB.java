cca50d596ef8b6c258d007e5e1bb389326ba418c
Barteks2x <Barteks2x@gmail.com>
2019-04-17 13:12:33 +0200

    Update to 1.9.4, compatible up to MC 1.12.2

--- src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
+++ src/main/java/com/github/barteks2x/fastflybreak/FFBB.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME)
+@Mod(modid = FFBB.MODID, version = FFBB.VERSION, name = FFBB.NAME, acceptedMinecraftVersions = "[1.9.4,1.12.2]")
 public class FFBB {
   public static final String VERSION = "${version}";
   public static final String NAME = "FastFlyBlockBreaking";
@@ -19,8 +19,8 @@ public class FFBB {
 
   @SubscribeEvent
   public void blockBreakSpeed(PlayerEvent.BreakSpeed event){
-    if(!event.entityPlayer.onGround && event.entityPlayer.capabilities.isFlying){
-      event.newSpeed = event.originalSpeed * 5;
+    if(!event.getEntityPlayer().onGround && event.getEntityPlayer().capabilities.isFlying){
+      event.setNewSpeed(event.getOriginalSpeed() * 5);
     }
   }
 }
